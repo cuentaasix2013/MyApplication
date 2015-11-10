68fad96888709783706ceedc49469a8dcfc0c97a
pauProva <paudam1990@gmail.com>
2015-11-10 09:30:24 +0100

    Ligao el boton al TextView

--- app/src/main/java/com/test/pau/myapplication/MainActivity.java
+++ app/src/main/java/com/test/pau/myapplication/MainActivity.java
@@ -4,13 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button) findViewById(R.id.button);
+        textView=(TextView)findViewById(R.id.textView);
+        button.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                textView.setText("Clicao!");
+
+            }
+         });
+
+
     }
 
     @Override
